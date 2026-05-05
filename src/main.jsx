d46520b4eafd7ad3edd763b3d9c7c172bd04ec77
PriscyAmore <angelaogwezhi02@gmail.com>
2026-05-05 12:56:26 +0100

    Remove index.css import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App.jsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
